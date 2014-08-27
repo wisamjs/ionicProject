4f1a8b0ddb97992a6f90a347a99931a9ef8432e5
wzaghal <wisamzaghal@gmail.com>
2014-08-27 10:45:43 -0400

    quick formatting update

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -2,14 +2,10 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl',function($scope, $log, Music){
 	$scope.getConcerts = Music.getConcerts()
+	.then(function(data){
 
-			.then(function(data){
-
-
-				//promise fulfilled
-				$scope.concerts =  data;
-
-			})
-			.then(null,$log.error);
+		//promise fulfilled
+		$scope.concerts =  data;
+	}).then(null,$log.error);
 
 });
