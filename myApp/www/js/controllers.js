56a332b419f768e0b26b3947861d7eafdc73437c
wzaghal <wisamzaghal@gmail.com>
2014-08-27 10:28:56 -0400

    create services for concert tab

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,5 +1,15 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl',function($scope){
+.controller('DashCtrl',function($scope, $log, Music){
+	$scope.getConcerts = Music.getConcerts()
+
+			.then(function(data){
+
+
+				//promise fulfilled
+				$scope.concerts =  data;
+
+			})
+			.then(null,$log.error);
 
 });
---
+++ myApp/www/js/services.js
@@ -0,0 +1,16 @@
+angular.module('starter.services',[])
+.constant('API_BASE_URL','http://ws.audioscrobbler.com/2.0/?format=json')
+.constant('API_KEY','&api_key=2d130fffdbb75b4388638d831bbc990f')
+
+.factory('Music', function($http, $log, API_BASE_URL,API_KEY){
+	var music = {};
+	music.getConcerts = function(){
+		return $http.get(API_BASE_URL+API_KEY+'&method=geo.getevents&location=toronto')
+			.then(function(response){
+				$log.info(response.data.events.event[0].image[3]['#text']);
+				return response.data.events.event
+			});
+	}
+
+	return music;
+});
