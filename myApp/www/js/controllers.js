92ec710fda23d9e5851e91cdfd880223bfff7a23
wzaghal <wisamzaghal@gmail.com>
2014-08-28 10:42:52 -0400

    move more logic to services

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -3,24 +3,34 @@ angular.module('starter.controllers', [])
 .controller('ConcertsCtrl',function($scope, $log, Music, userData){
 	var userConcerts = [];
 
-	$scope.getConcerts = Music.getConcerts()
+	$scope.getConcerts = userData.getAPIConcerts()
 	.then(function(data){
 
+		$log.info('data retrieved:' + data);
 		//promise fulfilled
+		userData.setAPIConcerts(data);
 		$scope.concerts =  data;
+
 	}).then(null,$log.error);
 
 	$scope.addToUser = function(concert){
+
 		return userData.toogleConcert(concert);
+
 	}
 
 	$scope.isSelected = function(concert){
+
 		return userData.isAttending(concert);
 	}
 })
 
 .controller('AttendingCtrl', function($scope, $log, Music, userData){
+
 	$scope.attendingConcerts = userData.getUserConcerts();
+	$log.info($scope.attendingConcerts);
+
+})
 
 	$log.info($scope.attendingConcerts);
 
--- myApp/www/js/services.js
+++ myApp/www/js/services.js
@@ -7,7 +7,6 @@ angular.module('starter.services',[])
 	music.getConcerts = function(){
 		return $http.get(API_BASE_URL+API_KEY+'&method=geo.getevents&location=toronto')
 			.then(function(response){
-				$log.info(response.data.events.event[0]);
 				return response.data.events.event
 			});
 	}
@@ -16,11 +15,30 @@ angular.module('starter.services',[])
 })
 
 
-.factory('userData', function($http, $log){
+.factory('userData', function(Music, $http, $log, $q){
 	var userConcerts = [];
+		apiConcerts = [];
 
 	return {
 
+		getAPIConcerts: function(){
+
+			$log.warn('All good?');
+			$log.warn(apiConcerts.length);
+
+			 if (apiConcerts.length > 0){
+			 		var deferred = $q.defer();
+					return $q.when(apiConcerts);
+
+			 }else{
+				return Music.getConcerts();
+			 }
+
+		},
+		setAPIConcerts: function(concerts){
+			apiConcerts = concerts;
+
+		},
 		toogleConcert: function(concert){
 			//remove concert if already selected
 			if (_.contains(userConcerts,concert)){
